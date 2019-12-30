b37d531800fa4556096686492c1111d499ca2c2d
abrahan <abrahan.chavez@frogslayer.com>
2019-12-30 00:31:46 -0600

    Adding build file

---
+++ build/precache-manifest.c4c3de4ee0e60becfe6762c25a032ce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8854618bf319283099036db1ac85ff97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "818a75558c96f94f841e",
+    "url": "/static/css/main.353e6241.chunk.css"
+  },
+  {
+    "revision": "fddaa0ca6bbc30492d43",
+    "url": "/static/js/2.ce134d53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ce134d53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "818a75558c96f94f841e",
+    "url": "/static/js/main.5a80f092.chunk.js"
+  },
+  {
+    "revision": "905c8360d2a306625d1b",
+    "url": "/static/js/runtime-main.630971f5.js"
+  },
+  {
+    "revision": "3deaf9eabe7fa2c1d633e0abb6145c5c",
+    "url": "/static/media/logo.3deaf9ea.svg"
+  }
+]);
